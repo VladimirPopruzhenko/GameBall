1a51b16b88cdb37bf96d97cbd8b30bed8d35cf76
VladimirPopruzhenko <73422582+VladimirPopruzhenko@users.noreply.github.com>
2022-07-28 23:07:27 +0300

    Update index.js

--- src/assets/js/game/index.js
+++ src/assets/js/game/index.js
@@ -21,7 +21,7 @@ const config = {
     physics: {
         default: 'matter',
         matter: {
-            debug: false,
+            debug: true,
             gravity: {
                 y: 0.9  
             },
